058db057b4edee4a09c4ef09476a0150e86156a9
Tomek <tomaszek92@gmial.com>
2017-04-18 12:43:20 +0200

    Some cleaning and add new test

---
+++ Library.BenchmarkDotNet/Benchmarks/Md5VsSha256Test.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+
+namespace Library.BenchmarkDotNet.Benchmarks
+{
+    public class Md5VsSha256Test
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public Md5VsSha256Test()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256()
+        {
+            return sha256.ComputeHash(data);
+        }
+
+        [Benchmark]
+        public byte[] Md5()
+        {
+            return md5.ComputeHash(data);
+        }
+    }
+}
--- Library.BenchmarkDotNet/Program.cs
+++ Library.BenchmarkDotNet/Program.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Text;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Columns;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Environments;
-using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Exporters.Csv;
-using BenchmarkDotNet.Exporters.Json;
-using BenchmarkDotNet.Jobs;
-using BenchmarkDotNet.Reports;
-using BenchmarkDotNet.Running;
+﻿using BenchmarkDotNet.Running;
+using Library.BenchmarkDotNet.Benchmarks;
 
 namespace Library.BenchmarkDotNet
 {
@@ -17,70 +7,28 @@ namespace Library.BenchmarkDotNet
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<StringBuildVsStringTest>(
-                ManualConfig
-                    .Create(new ManualConfig())
-                    .With(Job.Clr.With(Platform.X64))
-                    .With(HtmlExporter.Default)
-                    .With(TargetMethodColumn.Method)
-                    .With(StatisticColumn.Mean)
-                    .With(StatisticColumn.Median)
-                    .With(StatisticColumn.Min)
-                    .With(StatisticColumn.Max));
+            var benchmarkSwitcher = new BenchmarkSwitcher(new[]
+            {
+                typeof(Md5VsSha256Test),
+                typeof(StringBuilderVsStringTest)
+            });
+            benchmarkSwitcher.RunAll();
+
+            //BenchmarkRunner.Run<Md5VsSha256Test>();
+            //BenchmarkRunner.Run<StringBuilderVsStringTest>();
+
+            //BenchmarkRunner.Run<StringBuildVsStringTest>(
+            //    ManualConfig
+            //        .Create(new ManualConfig())
+            //        .With(Job.Clr.With(Platform.X64))
+            //        .With(HtmlExporter.Default)
+            //        .With(TargetMethodColumn.Method)
+            //        .With(StatisticColumn.Mean)
+            //        .With(StatisticColumn.Median)
+            //        .With(StatisticColumn.Min)
+            //        .With(StatisticColumn.Max));
 
             //BenchmarkRunner.Run<StringBuildVsStringTest>(new StringBuildVsStringTestConfig());
-        }
-    }
-
-    public class StringBuildVsStringTest
-    {
-        private readonly int _stringLength = 10000;
-
-        [Benchmark]
-        public string GenerateStringByString()
-        {
-            string result = "";
-            for (int i = 0; i < _stringLength; i++)
-            {
-                result += "A";
-            }
-            return result;
-        }
-
-        [Benchmark]
-        public string GenerateStringByStringBuilder()
-        {
-            StringBuilder result = new StringBuilder();
-            for (int i = 0; i < _stringLength; i++)
-            {
-                result.Append("A");
-            }
-            return result.ToString();
-        }
-    }
-
-    public class StringBuildVsStringTestConfig : ManualConfig
-    {
-        public StringBuildVsStringTestConfig()
-        {
-            // Jobs
-            Add(Job.Clr.With(Platform.X64));
-
-            // Exporters
-            Add(CsvExporter.Default);
-            Add(HtmlExporter.Default);
-            Add(MarkdownExporter.GitHub);
-            Add(PlainExporter.Default);
-            Add(JsonExporter.Brief);
-            Add(JsonExporter.Full);
-
-            // Columns
-            Add(TargetMethodColumn.Method);
-            Add(StatisticColumn.Mean);
-            Add(StatisticColumn.Median);
-            Add(StatisticColumn.Min);
-            Add(StatisticColumn.Max);
-            Add(StatisticColumn.OperationsPerSecond);
         }
     }
 }
